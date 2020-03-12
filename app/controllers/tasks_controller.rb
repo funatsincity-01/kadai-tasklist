c425c4a630d5019d9d62e660437e908962b8535a
yoshikobo <yoshikobo@yahoo.co.jp>
2020-03-12 12:32:48 +0000

    20200312 課題レビュー後

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,19 @@
 class TasksController < ApplicationController
-    before_action :require_user_logged_in, only: [:index, :show]
-    def index
-        #@tasks = Task.all
+    # before_action :require_user_logged_in, only: [:index, :show]
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:edit, :update, :destroy, :show]
     
-        if logged_in?
-            @task = current_user.tasks.build  # form_with 用
+    
+    def index
+        #if logged_in?
+        #    @task = current_user.tasks.build  # form_with 用
             @tasks = current_user.tasks.order(id: :desc).page(params[:page])
-        end
+        #end
     end
     
     def show
         @task = Task.find(params[:id])
+
     end
     
     def new
@@ -33,6 +36,7 @@ class TasksController < ApplicationController
     
     def edit
         @task = Task.find(params[:id])
+        # @task = current_user.tasks.find_by(id:params[:id])
     end
     
     
@@ -66,5 +70,14 @@ private
     def task_params
         params.require(:task).permit(:content,:status)
     end
+
+
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
+    end
+
     
 end
